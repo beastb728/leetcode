2203fa8786e7b43c8f3b53a3b64c638fe20ab2a2
Ishaan Singh <515.ishaansingh@gmail.com>
2026-08-26 20:48:09 +0530

    Time: 4 ms (10.68%), Space: 71.7 MB (52.88%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int prefix = 0;
+        int minPrefix = 0;
+        int ans = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefix = prefix + nums[i];
+            ans = max(ans, prefix - minPrefix);
+            minPrefix = min(minPrefix, prefix);
+        }
+
+        return ans;
+    }
+};
